394cbfaacdb5c556b7d88da8b82b4bcb542c7dc0
Marina de Sousa <160603203+marinadesousa@users.noreply.github.com>
2024-03-07 14:35:56 +0000

    Aprendendo JavaScript

---
+++ temas.js
@@ -0,0 +1,32 @@
+// DOM - Document Object Model - Modelo de Objeto do Documento
+// Transforma tudo na página em objetos. Cada objeto recebe atributos (propriedades) e métodos (funções) associadas a ele.
+
+//FUNÇÕES
+// function nomeDaFunção(parâmetros) { 
+//      corpo da função ( o que ela vai fazer com os parametros de entrada 
+// toda funçao tem [corpo da função])
+
+// Saber o tema atual e obter o elemento do tema atual
+
+// Saber onde está o atributo de tema
+
+// Se o botão for clicado, então...
+// addEventListener(evento,função)
+document.querySelector('#btTrocatema').addEventListener('click', 
+() => {
+
+    // pegar o elemento com o tema
+    // se o tema for light, então mude o atributo para dark
+    // se não, então mude o atributo para light
+
+    if (document.documentElement.getAttribute('data-tema') === 
+    'light') {
+        document.documentElement.setAttribute('data-tema', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-tema', 'light');
+    }
+});
+// Trocar o tema:
+// 1. Pegar o elemento com tema 
+// 2. Apagar o atributo do tema atual
+// 3. Escrever o atributo do novo tema
